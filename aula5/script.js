74857448e985e2bddbe533640eb66f38c1270406
Vitoria Sousa <maria.v.cosmo@gmail.com>
2024-04-26 10:35:56 -0300

    aula5

---
+++ aula5/script.js
@@ -0,0 +1,25 @@
+// string
+let nome = "Vitoria" 
+
+// number
+let idade = 90
+
+//Boolean - true
+let logado = true
+
+//Boolean - false
+logado = false
+
+// array 
+let lista = ['ovo', 'massa', 'corante']
+
+// objeto
+let nomeCompleto = { nome: 'Vitoria', idade: 18 }
+
+
+// Comentários
+
+/*Este é um comentário
+de várias linhas
+só porque eu eu quero.*/
+console.log(nome)
