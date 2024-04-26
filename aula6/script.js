4e556cb4ccdc7b917a2bef3384d416f38e6ddfaa
Vitoria Sousa <maria.v.cosmo@gmail.com>
2024-04-26 11:21:28 -0300

    aula6

---
+++ aula6/script.js
@@ -0,0 +1,18 @@
+// 1. Crie uma variável para armazenar o Name
+//  do carro e ponha o valor ferrari. E exiba-a.  
+
+let carro = 'Ferrari'
+console.log('Nome do carro:', carro)
+
+// 2. Crie uma variável para armazenar o preço
+// do bolo, que custa R$ 19,99. E exiba-a.
+
+let precoBolo = 19.99
+console.log('Preço do bolo: R$',precoBolo)
+
+
+// 3. Corrija o código a seguir (e exiba-o).
+// let "cidade" = São Paulo
+
+let cidade = 'São Paulo'
+console.log('Cidade:', cidade)
