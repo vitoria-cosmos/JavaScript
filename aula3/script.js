301c8b2e5988fe9f3783a4d91da60e2d0243b94e
Vitoria Sousa <maria.v.cosmo@gmail.com>
2024-04-26 09:23:11 -0300

    aula3

--- aula3/script.js
+++ aula3/script.js
@@ -9,3 +9,11 @@ console.log('Opa, tudo bem?')
 console.log('Idade:',idade)
 
 console.log('Nome:',nome)
+
+// Cidade
+let cidade = 'Maracanaú'
+console.log('Minha cidade é:', cidade)
+
+// Estado
+let estado = 'Ceará'
+console.log('Meu estado é:', estado)
