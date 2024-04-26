364e6e3519f68fb0563c5bd1dbb7c3213513cdb6
Vitoria Sousa <maria.v.cosmo@gmail.com>
2024-04-26 09:47:15 -0300

    aula4

---
+++ aula4/script.js
@@ -0,0 +1,23 @@
+// String
+let nome = "Vitoria" 
+
+// Number
+let idade = 90
+
+// true
+let logado = true
+
+// false
+logado = false
+
+// array 
+let lista = ['ovo', 'massa', 'corante']
+
+// objeto
+let nomeCompleto = { nome: 'Vitoria', idade: 18 }
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof nomeCompleto)
