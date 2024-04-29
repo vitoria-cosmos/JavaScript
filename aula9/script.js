4c3112e5a032470e3ae2d0b0f1ff0b8532cb7c9d
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-04-29 15:26:52 -0300

    aula9

---
+++ aula9/script.js
@@ -0,0 +1,21 @@
+let nome = 'Vitoria';
+let sobrenome = 'Cosmo';
+
+// let nomeCompleto = nome + ' ' + sobrenome;
+{}
+let nomeCompleto = `${nome} ${sobrenome}`
+
+console.log('Nome: ' + nomeCompleto);
+
+
+let idade = 18;
+// let idadeString = `${idade} anos.`;
+// let idadeString = `${idade + 2} anos.`;
+// let idadeString = `${idade + 'bla'} anos.`;
+let idadeString = 'Idade: '  + idade + ' anos.'
+
+// let idadeString = `Idade: ${idade} anos.`;
+
+
+
+console.log(idadeString);
