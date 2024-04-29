f1a383a74c826a132d4fcfff53e4b71da407f32b
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-04-29 14:49:46 -0300

    aula8

---
+++ aula8/script.js
@@ -0,0 +1,39 @@
+
+// Operações com number 
+// let x = 2;
+
+// // x = x - 8;
+
+// // x = x * 8;
+
+// x = x / 8;
+
+// console.log(x);
+
+// Operação com string 
+// let x = '2';
+// x = x + 8;
+
+// console.log(typeof x);
+
+// let nome = 'Vitoia';
+// let sobrenome = 'Cosmo';
+// // let nomeCompleto = nome + sobrenome;
+// let nomeCompleto = nome + ' ' + sobrenome;
+
+// console.log(nomeCompleto);
+
+// let n1 = 15;
+// let n2 = 20;
+// let n3 = 10;
+
+// // let soma = n1 + n2 + n3;
+
+// let soma = (n1 + n2) - n3;
+
+// console.log(soma);
+
+let numero = '10';
+let numero2 = 10;
+
+
