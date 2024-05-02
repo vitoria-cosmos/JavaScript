26b85c757f5b9eec286869eca32eb758bed77e9a
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-05-02 08:13:24 -0300

    aula14

---
+++ aula14/script.js
@@ -0,0 +1,42 @@
+// let idade = 14;
+
+// if (idade > 18) { - isso é uma verificação de boolean.
+
+// }
+
+// let ok = true;
+// let ok = false;
+
+// if (ok) {
+//     console.log('Entrou no IF');
+// } else {
+//     console.log('Não entrou no IF');
+// }
+
+// Outro exemplo: Fazendo verificação
+//  de boolean em uma linha só -----
+
+let idade = 14;
+
+let verificacao = idade > 18; // Devolve verdadeiro ou falso.
+
+// if (verificacao) {
+//     console.log('Entrou no IF.');
+// } else {
+//     console.log('Não entrou no IF.');
+// }
+{}
+
+// let adulto = idade > 18;
+
+// if (adulto) {
+//     console.log('É adulto.');
+// } else {
+//     console.log('Não é adulto.');
+// }
+
+console.log(idade > 18);
+
+if (true) {
+    console.log('ENTROU');
+}
