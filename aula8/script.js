ca6e9dd7b5c911f1fabcc31af65f1af7df15cd0f
Vitoria Sousa <maria.v.cosmo@gmail.com>
2024-05-13 22:43:57 -0300

    aula08

---
+++ aula8/script.js
@@ -0,0 +1,20 @@
+// let
+// let nome = 'Vitoria';
+// console.log(nome)
+
+// nome = 'Pedro';
+// console.log(nome)
+
+// var
+// var nome = "Vitoria";
+// console.log(nome)
+
+// nome = "João"
+// console.log(nome)
+
+// const
+const nome = 'Boniek';
+console.log(nome)
+
+nome = 'João'
+console.log(nome)
