b81dde5f6ac538f7fd4a62bf07f9152403435f5a
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-18 13:24:03 -0300

    aula21

---
+++ aula21/script.js
@@ -0,0 +1,15 @@
+// function somar(n1, n2) {
+//     let resultado = n1 + n2;
+//     console.log('Resultado:', resultado);
+// }
+
+// somar(10, 15);
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+}
+
+let completo = nomeCompleto('Vitoria', 'Cosmo');
+console.log('Completo: ', completo);
+
+// ENTRADA -> PROCESSAMENTO -> SAÍDA
