35b657be38f25942ffcb3cc8ff663214cb42918e
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-25 19:25:08 -0300

    ex02 finalizado

---
+++ aula22/ex02/exercicio2.js
@@ -0,0 +1,35 @@
+/* 
+Calcule o preço do imóvel
+ - m2 = 3.000
+ - Se tiver 1 quarto, o m2 é 1x
+ - Se tiver 2 quartos, o m2 é 1.2x
+ - Se tiver 3 quartos, o m2 é 1.5x
+
+ Uso da função:
+
+*/
+
+function calcularImovel(metro, aposentos) {
+    let m2 = 3.000;
+    let valorTot;
+    switch (aposentos) {
+        case 1:
+            m2 = m2;
+            valorTot = m2 * metro;
+            break;
+        case 2:
+            m2 = m2 * 1.2;
+            valorTot = m2 * metro;
+            break;
+        case 3:
+            m2 = m2* 1.5;
+            valorTot = m2 * metro;
+            break; 
+    }
+    return valorTot;
+}
+
+let metragem = 123;
+let quartos = 3;
+let preco = calcularImovel(metragem, quartos);
+console.log(`A casa custa R$ ${preco}`);
