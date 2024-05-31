c6b900cb98e0ada846c214a5200e3f1ca180427f
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-31 13:31:40 -0300

    escopo global e local

---
+++ aula24/script.js
@@ -0,0 +1,33 @@
+// function add() {
+//     let count = 0;
+
+//     count++;
+// }
+
+// add();
+// add();
+
+// console.log(count);
+
+// let count = 0;
+
+// function add() {
+//     count++;
+// }
+
+// add();
+// add();
+
+// console.log(count);
+
+let count = 0;
+
+function add() {
+    let count = 0; // variavel escopo local 
+    count++;
+}
+
+add();
+add();
+
+console.log(count);
