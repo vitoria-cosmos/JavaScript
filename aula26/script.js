ac62ae7f7c8aabeecd5c8c119ae76b900079b323
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-06-14 12:16:52 -0300

    introdução a arrays

---
+++ aula26/script.js
@@ -0,0 +1,36 @@
+// um valor apenas
+let color = 'blue';
+
+// array
+
+let colors = ['blue', 'red', 'green'];
+
+console.log(colors);
+
+console.log(colors[1]);
+
+// array de idades
+
+let idades = [10, 20, 30];
+
+// array com mais de um tipo de dado
+
+let lista = ['algo', 30, 'blabla', true];
+
+//  array com array dentro dela
+
+let nomes = ['bonieky', 'pedro', 'silvia'];
+
+let lista1 = ['blabla', nomes];
+
+let lista2 = ['blabla', ['xx', 'yy']];
+console.log(lista[1][0]);
+
+let segundo = lista[1];
+
+console.log(segundo[0]);
+
+let lista3 = [
+    'blabla',
+    ['xx', 'yy']
+]
